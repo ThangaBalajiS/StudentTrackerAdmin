f06cfe26976c09256c3080ae3057c6a8f2987726
Thanga Balaji S <thangabalajis@gmail.com>
2018-04-11 07:44:17 +0530

    login module added

---
+++ app/src/main/java/com/thangabalajis/studenttrackeradmin/Login.java
@@ -0,0 +1,34 @@
+package com.thangabalajis.studenttrackeradmin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText etUname,etPw;
+    final String USER_NAME = "sankar";
+    final String PASS_WORD = "admin123";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUname = findViewById(R.id.un);
+        etPw = findViewById(R.id.pw);
+
+    }
+
+    public void login(View view){
+        if (etUname.getText().toString() == USER_NAME && etPw.getText().toString() == PASS_WORD ){
+            Intent i = new Intent(this,MainActivity.class);
+            startActivity(i);
+        }else{
+            Toast.makeText(this,"Username or password incorrect",Toast.LENGTH_LONG).show();
+        }
+    }
+}
